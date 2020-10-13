22a40ad7bd7b29ca7d2b06c4589dbe2ca0ca7607
phuclh@minerva.vn <hoangphuc98>
2020-10-13 13:41:26 +0700

    -- count down timer

---
+++ app/src/main/java/hphuc5453/custom_library/Utils.kt
@@ -0,0 +1,26 @@
+package hphuc5453.custom_library
+
+class Utils {
+
+    fun milliSecondsToTimer(milliseconds: Long): String? {
+        var finalTimerString = ""
+        var secondsString = ""
+        // Convert total duration into time
+        val hours = (milliseconds / (1000 * 60 * 60)).toInt()
+        val minutes = (milliseconds % (1000 * 60 * 60)).toInt() / (1000 * 60)
+        val seconds = (milliseconds % (1000 * 60 * 60) % (1000 * 60) / 1000).toInt()
+        // Add hours if there
+        if (hours > 0) {
+            finalTimerString = "$hours:"
+        }
+        // Prepending 0 to seconds if it is one digit
+        secondsString = if (seconds < 10) {
+            "0$seconds"
+        } else {
+            "" + seconds
+        }
+        finalTimerString = "$finalTimerString$minutes:$secondsString"
+        // return timer string
+        return finalTimerString
+    }
+}
